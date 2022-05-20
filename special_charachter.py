adeaf84d718de895d5f7b8bb915ddddb8f95a88b
eternalb9 <105520383+eternalb9@users.noreply.github.com>
2022-05-20 23:15:02 +0530

    Update special_charachter.py

--- special_charachter.py
+++ special_charachter.py
@@ -1,7 +1,17 @@
+
+
+
 str = input("Enter a string : ")
 
-if str.isalnum()==1:
-    print(" The string is acceptable. ")
+length=len(str)
+for i in range(length-1):
+    if str[i].isalnum()==1 or str[i].isspace()==1:
+        i=i+1
+        
 
-else:
-    print(" The string is not acceptable.")    
+    else:
+        print(" The string is not acceptable.")
+           
+
+if i==length-1:
+    print("The string is acceptable")
